ba33ead306d1a9d446e5bdc52b4f3806afc2a189
guburchardt <guburchardt@hotmail.com>
2025-05-05 14:49:56 -0300

    Adding Dynamic Data

--- payments/pix.py
+++ payments/pix.py
@@ -16,10 +16,10 @@ class Pix:
         img = qrcode.make(hash_payment)
 
         # save image as png file
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
-
+        file_name = f"qr_code_payment_{bank_payment_id}"
+        img.save(f"static/img/{file_name}.png")
 
         return {
             "bank_payment_id": bank_payment_id,
-            "qr_code_path": f"static/img/qr_code_payment_{bank_payment_id}."
+            "qr_code_path": file_name
         }
