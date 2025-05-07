ea7c1c0c604a143ae4b57bf73a1503872a753aa6
guburchardt <guburchardt@hotmail.com>
2025-05-07 12:17:49 -0300

    Implementing payment confirmation

--- db_models/payment.py
+++ db_models/payment.py
@@ -7,15 +7,14 @@ class Payment(db.Model):
     paid = db.Column(db.Boolean, default=False)
     bank_payment_id = db.Column(db.String(200), nullable=True)
     qr_code = db.Column(db.String(100), nullable=True)
-    expiration_date = db.Column(db.DateTime) #2024-01-01 01:01:01
+    expiration_date = db.Column(db.DateTime)
 
     def to_dict(self):
         return {
             "id": self.id,
             "value": self.value,
-            "paid": self.paid, 
+            "paid": self.paid,
             "bank_payment_id": self.bank_payment_id,
             "qr_code": self.qr_code,
-            "expiration_date": self.expiration_date
+            "expiration_date": self.expiration_date,
         }
-
